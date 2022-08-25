9830ade050af9a09daf8abf1456f7dedb857a586
Alexey Nikitin <alexey.nikitin6723@gmail.com>
2022-08-25 19:35:58 +0400

    add: button to save card

--- js/script.js
+++ js/script.js
@@ -5,10 +5,24 @@ const cardImage = document.querySelector('.card__image');
 const cardText = document.querySelector('.card__text');
 const buttonText = document.querySelector('.header__button-change_text');
 const buttonImage = document.querySelector('.header__button-change_image');
+const cardLink = document.querySelector('.card__link');
+
 
 const state = {
 	gender: body.classList.contains('women') ? 'women' : 'men',
 };
+
+const saveCard = async () => {
+	html2canvas(cardWrapper).then(canvas => {
+		cardLink.textContent = 'Скачать карточку';
+		canvas.style.maxWidth = "100%";
+		canvas.style.height = "auto";
+		canvas.style.display = "none";
+		cardLink.append(canvas);
+		cardLink.setAttribute('href', canvas.toDataURL('image/png'));
+		cardLink.setAttribute('download', 'happy-birthday.png');
+	})
+}
 
 const getRandomForArr = (arr) => {
 	const randomNumber = Math.floor(Math.random() * arr.length);
@@ -32,6 +46,7 @@ const getDataToCard = () => {
 		state.text = getRandomForArr(data.text[state.gender]);
 		state.photo = getRandomForArr(data.photo[state.gender]);
 		changeDOM();
+		saveCard();
 	});
 }
 
@@ -72,3 +87,22 @@ buttonWomen.addEventListener('click', changeToWomen);
 buttonText.addEventListener('click', changeText);
 buttonImage.addEventListener('click', changeImage);
 getDataToCard();
+
+const cardWrapper = document.querySelector('.card__wrapper');
+
+// Вариант с открытием в новом окне
+// cardWrapper.addEventListener('dblclick', () => {
+// 	const newWindow = window.open(
+// 		'',
+// 		'',
+// 		`width=840,height=520,top=${(screen.height / 2) - 520 / 2},left=${(screen.width / 2) - 840 / 2}`);
+// 	html2canvas(cardWrapper).then(canvas => {
+// 		canvas.style.maxWidth = `100%`;
+// 		canvas.style.height = 'auto';
+// 		newWindow.document.body.append(canvas);
+// 	})
+// })
+
+cardLink.addEventListener('click', () => {
+	saveCard();
+})
